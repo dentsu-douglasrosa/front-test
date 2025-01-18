35e7ea06cc56d9f6e6c468cf838840a494b7c27b
Douglas Rosa <douglas.rosa@em.com>
2025-01-18 01:12:00 -0300

    DWS :: PostDetails :: Include all information on the page

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/post-details.scss';
 import { usePostDetails } from '../hooks/usePostDetails'
 import Posts from './Posts';
+import moment from 'moment';
 
 const PostDetails = (): JSX.Element => {
   const { state, controller } = usePostDetails();
@@ -10,11 +11,36 @@ const PostDetails = (): JSX.Element => {
 
   return (
     <div className="posts-details-container">
-        <h1>{state.post.title}</h1>
-        <img src={state.post.thumbnail_url} alt={state.post.title} className="post-details-thumbnail" />
-        <div>
-          <Posts orderBy={"updatedAt"} limit={3} />
-        </div>
+      <div className="post-header">
+          <h1 className="post-title">{state.post.title}</h1>
+      </div>
+      <div className="post-meta">
+          <div className="author">
+              <img className="author-profile-picture" src={state.post.author.profilePicture} alt={state.post.title} />
+              <div>
+                <span className="author-name">{state.post.author.name}</span>
+                <div className="dates">
+                    <span className="updated-date">{moment(new Date(state.post.updatedAt)).format("MMM DD, YYYY")}</span>
+                </div>
+              </div>
+          </div>
+      </div>
+      <div className='post-thumbnail-container'>
+        <img className="post-thumbnail" src={state.post.thumbnail_url} alt={state.post.title} />
+      </div>
+      <div className="post-content">
+          <p className="content-text">
+              {state.post.content}
+          </p>
+      </div>
+      <div className="post-categories">
+      {state.post.categories.map(cat => {
+        return <span key={`post-category-${cat.id}`} className="category">{cat.name}</span>
+      })}
+      </div>
+      <div>
+        <Posts orderBy={"updatedAt"} limit={3} />
+      </div>
     </div>
   );
 }
