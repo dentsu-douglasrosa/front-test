809d08f26fd470c2c97a61ba23bc0a7940e9962e
Douglas Rosa <douglas.rosa@em.com>
2025-01-21 12:04:56 -0300

    DWS :: Sidebar adjustments

--- src/hooks/useSidebar.ts
+++ src/hooks/useSidebar.ts
@@ -1,9 +1,11 @@
 import { UseSidebarReturn, UseSidebarProps } from "src/types/sidebar.type";
 import { useNavigate, useParams } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
 
 export const useSidebar = (_?: UseSidebarProps): UseSidebarReturn => {
     const { id } = useParams<{ id: string }>();
     const isOnPostDetails = !!id
+    const { t } = useTranslation();
 
     const navigate = useNavigate()
 
@@ -14,6 +16,7 @@ export const useSidebar = (_?: UseSidebarProps): UseSidebarReturn => {
     return {
         state: {
             isOnPostDetails,
+            labelBack: t('back'),
         },
         controller: {
             onClickBack,
